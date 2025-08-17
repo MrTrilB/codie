1418cb94557679a1d7352e362a48efd497adf22e
MrTrilB <anthony.bluthner@outlook.com>
2025-08-17 12:15:01 +0100

    fixed the exstension icon not appearing in the sidebar

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,47 @@
+class CodieChatViewProvider implements vscode.WebviewViewProvider {
+	public static readonly viewType = 'codie-chat-view';
+	private chatManager: ChatManager;
+	constructor(private readonly context: vscode.ExtensionContext) {
+		this.chatManager = new ChatManager();
+	}
+
+	resolveWebviewView(webviewView: vscode.WebviewView) {
+		webviewView.webview.options = {
+			enableScripts: true,
+			localResourceRoots: [
+				vscode.Uri.file(path.join(this.context.extensionPath, 'media'))
+			]
+		};
+		const mediaPath = path.join(this.context.extensionPath, 'media');
+		const htmlPath = path.join(mediaPath, 'index.html');
+		const cssUri = webviewView.webview.asWebviewUri(vscode.Uri.file(path.join(mediaPath, 'chat.css')));
+		const jsUri = webviewView.webview.asWebviewUri(vscode.Uri.file(path.join(mediaPath, 'main.js')));
+		import('fs').then(fs => {
+			fs.readFile(htmlPath, 'utf8', (err, data) => {
+				if (err) {
+					webviewView.webview.html = `<body><h2>Failed to load Codie UI</h2><pre>${err.message}</pre></body>`;
+					return;
+				}
+				let html = data
+					.replace('./chat.css', cssUri.toString())
+					.replace('./main.js', jsUri.toString());
+				webviewView.webview.html = html;
+			});
+		});
+		webviewView.webview.onDidReceiveMessage(async (msg) => {
+			if (msg.type === 'sendMessage') {
+				const aiMsg = await this.chatManager.sendMessage(msg.text);
+				webviewView.webview.postMessage({ type: 'aiMessage', message: aiMsg });
+			} else if (msg.type === 'getHistory') {
+				webviewView.webview.postMessage({ type: 'history', history: this.chatManager.getHistory() });
+			} else if (msg.type === 'setProvider') {
+				this.chatManager.setProvider(msg.providerId);
+			} else if (msg.type === 'getProviders') {
+				webviewView.webview.postMessage({ type: 'providers', providers: this.chatManager.getProviderList() });
+			}
+		});
+	}
+}
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
@@ -7,6 +51,11 @@ import { ChatManager } from './chatManager';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	// Register Codie activity bar view provider
+	const codieChatViewProvider = new CodieChatViewProvider(context);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(CodieChatViewProvider.viewType, codieChatViewProvider)
+	);
 	// Hello World command (keep for now)
 	const helloDisposable = vscode.commands.registerCommand('codie.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from Codie!');
